82c16d830ef74abdd2e8ae0a9bd126e5ce093289
Nguyễn Minh Phúc <phuc@lhu.edu.vn>
2025-08-26 13:14:18 +0000

    bố trí layout laij cho hợp ý

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -146,8 +146,8 @@ export default function Dashboard() {
             </div>
           </header>
           <main className="container mx-auto p-4 sm:p-6 lg:p-8">
-            <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
-              <div className="lg:col-span-1 space-y-8">
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+              <div className="space-y-8">
                 <CustomizationPanel
                   customization={customization}
                   setCustomization={setCustomization}
@@ -156,7 +156,7 @@ export default function Dashboard() {
                 />
                 <EmbedGuide chatbotId={user.uid} />
               </div>
-              <div className="lg:col-span-2">
+              <div>
                  <Card>
                     <ChatbotPreview
                       customization={customization}
