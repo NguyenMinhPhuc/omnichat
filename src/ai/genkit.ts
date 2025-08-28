4e94566e1c0282e1a256e8ecc0af59ad3a7a0425
Nguyễn Minh Phúc <phuc@lhu.edu.vn>
2025-08-28 08:44:46 +0000

    TypeError: (0 , {imported module [project]/nodemodules/genkit/lib/common

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,8 +1,9 @@
+
 'use server';
-import * as genkit from 'genkit';
+import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
-export const ai = genkit.configureGenkit({
+export const ai = genkit({
   plugins: [
     googleAI({
       apiKey: process.env.GEMINI_API_KEY,
