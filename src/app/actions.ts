1438fd963da1f6fed1b06c41f28f30a14067f76a
Nguyễn Minh Phúc <phuc@lhu.edu.vn>
2025-09-03 05:21:32 +0000

    failed to parse private key" Error Too few bytes to read asn.1 value vẫn

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -10,7 +10,7 @@ const serviceAccount = {
   type: 'service_account',
   project_id: 'omnichat-isnkq',
   private_key_id: '3145ffc8282362b5d43e2f75471d2b7d59837941',
-  private_key: '-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDKtPMgN/P3xMZu\nANl9N1ADgYjA+oVwPZ/vR2pA5L5fV6w7g6d7b2a7b3c2I5a6l5n7Z9Z7p3w4M5V1B7\nb2a7b3c2I5a6l5n7Z9Z7p3w4M5V1B7b2a7b3c2I5a6l5n7Z9Z7p3w4M5V1B7b2a7\nb3c2I5a6l5n7Z9Z7p3w4M5V1B7b2a7b3c2I5a6l5n7Z9Z7p3w4M5V1B7b2a7b3c2\nI5a6l5n7Z9Z7p3w4M5V1B7b2a7b3c2I5a6l5n7Z9Z7p3w4M5V1B7b2a7b3c2I5a6\nl5n7Z9Z7p3w4M5V1B7b2a7b3c2I5a6l5n7Z9Z7p3w4M5V1BwIDAQAB\nAoIBAQC4b6c3f6e4a3b2a1e0f9a8b7c6d5e4f3a2b1c0d9e8f7a6b5c4d3e2f1\na0b9c8d7e6f5a4b3c2d1e0f9a8b7c6d5e4f3a2b1c0d9e8f7a6b5c4d3e2f1a0\nb9c8d7e6f5a4b3c2d1e0f9a8b7c6d5e4f3a2b1c0d9e8f7a6b5c4d3e2f1a0b9\nc8d7e6f5a4b3c2d1e0f9a8b7c6d5e4f3a2b1c0d9e8f7a6b5c4d3e2f1a0b9c8\nd7e6f5a4b3c2d1e0f9a8b7c6d5e4f3a2b1c0d9e8f7a6b5c4d3e2f1a0b9c8d7\ne6f5a4b3c2d1e0f9a8b7c6d5e4f3a2b1c0d9e8f7a6b5c4d3e2f1a0b9c8d7e6\nf5a4b3c2d1e0f9a8b7c6d5e4f3a2b1c0d9e8f7a6b5c4d3e2f1a0\n-----END PRIVATE KEY-----\n'.replace(/\\n/g, '\n'),
+  private_key: '-----BEGIN PRIVATE KEY-----\\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDKtPMgN/P3xMZu\\nANl9N1ADgYjA+oVwPZ/vR2pA5L5fV6w7g6d7b2a7b3c2I5a6l5n7Z9Z7p3w4M5V1B7\\nb2a7b3c2I5a6l5n7Z9Z7p3w4M5V1B7b2a7b3c2I5a6l5n7Z9Z7p3w4M5V1B7b2a7\\nb3c2I5a6l5n7Z9Z7p3w4M5V1B7b2a7b3c2I5a6l5n7Z9Z7p3w4M5V1B7b2a7b3c2\\nI5a6l5n7Z9Z7p3w4M5V1B7b2a7b3c2I5a6l5n7Z9Z7p3w4M5V1B7b2a7b3c2I5a6\\nl5n7Z9Z7p3w4M5V1B7b2a7b3c2I5a6l5n7Z9Z7p3w4M5V1BwIDAQAB\\nAoIBAQC4b6c3f6e4a3b2a1e0f9a8b7c6d5e4f3a2b1c0d9e8f7a6b5c4d3e2f1\\na0b9c8d7e6f5a4b3c2d1e0f9a8b7c6d5e4f3a2b1c0d9e8f7a6b5c4d3e2f1a0\\nb9c8d7e6f5a4b3c2d1e0f9a8b7c6d5e4f3a2b1c0d9e8f7a6b5c4d3e2f1a0b9\\nc8d7e6f5a4b3c2d1e0f9a8b7c6d5e4f3a2b1c0d9e8f7a6b5c4d3e2f1a0b9c8\\nd7e6f5a4b3c2d1e0f9a8b7c6d5e4f3a2b1c0d9e8f7a6b5c4d3e2f1a0b9c8d7\\ne6f5a4b3c2d1e0f9a8b7c6d5e4f3a2b1c0d9e8f7a6b5c4d3e2f1a0b9c8d7e6\\nf5a4b3c2d1e0f9a8b7c6d5e4f3a2b1c0d9e8f7a6b5c4d3e2f1a0\\n-----END PRIVATE KEY-----\\n',
   client_email: 'firebase-adminsdk-h1i2j@omnichat-isnkq.iam.gserviceaccount.com',
   client_id: '114170669287340698269',
   auth_uri: 'https://accounts.google.com/o/oauth2/auth',
@@ -22,10 +22,14 @@ const serviceAccount = {
 let db: Firestore;
 
 if (!getApps().length) {
-  const app: App = initializeApp({
-    credential: cert(serviceAccount),
+  const anApp: App = initializeApp({
+    credential: cert({
+        projectId: serviceAccount.project_id,
+        clientEmail: serviceAccount.client_email,
+        privateKey: serviceAccount.private_key.replace(/\\n/g, '\n')
+    }),
   });
-  db = getFirestore(app);
+  db = getFirestore(anApp);
 } else {
   // Use the existing app instance
   db = getFirestore();
