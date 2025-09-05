1c76ebf1c658be90e087f95d3b38fcbd48a8fbab
Nguyễn Minh Phúc <phuc@lhu.edu.vn>
2025-09-05 01:07:01 +0000

    vẫn không được

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -4,7 +4,7 @@ import { googleAI } from '@genkit-ai/googleai';
 
 export const ai = genkit({
   plugins: [
-    googleAI({ apiKey: process.env.GEMINI_API_KEY || 'AIzaSyDvmM-Xd6GUcAn3iHoPqt38DF5WA98YoYY' }),
+    googleAI({ apiKey: process.env.GEMINI_API_KEY }),
   ],
 });
 // AIzaSyDvmM-Xd6GUcAn3iHoPqt38DF5WA98YoYY này là một API key giả lập, không sử dụng được.
